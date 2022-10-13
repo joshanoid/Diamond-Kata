1de837fbf30562799c33e9fd6ea996e9578f4ed6
Jozsef Varga <joshanoid@gmail.com>
2022-10-13 18:29:16 +0200

    finished diamond generator utility and it's tests

--- src/pages/Main/utils.test.ts
+++ src/pages/Main/utils.test.ts
@@ -1,4 +1,27 @@
-import { generateDiamond } from "./utils";
+import { generateDiamond, addLine } from "./utils";
+
+describe("Generate Line", () => {
+    it("returns empty string if the difference of index and length is smaller than 0", () => {
+        expect(addLine(2, 1)).toBe("");
+        expect(addLine(0, -10)).toBe("");
+        expect(addLine(10, 6)).toBe("");
+    });
+
+    it("returns empty string if index is smaller than 0", () => {
+        expect(addLine(-2, 1)).toBe("");
+        expect(addLine(-1, -10)).toBe("");
+    });
+
+    it("returns A for zero index", () => {
+        expect(addLine(0, 1)).toBe("A");
+        expect(addLine(0, 5)).toBe("    A");
+    });
+
+    it("returns proper lines", () => {
+        expect(addLine(3, 6)).toBe("  D     D");
+        expect(addLine(2, 7)).toBe("    C   C");
+    });
+});
 
 describe("Generate Diamond", () => {
     it("returns empty string if the input is an empty string", () => {
@@ -7,5 +30,15 @@ describe("Generate Diamond", () => {
 
     it("returns A if the letter is A", () => {
         expect(generateDiamond("A")).toBe("A");
+    });
+
+    it("generates proper diamonds", () => {
+        expect(generateDiamond("B").replace(new RegExp("\r?\n", "g"), "&")).toBe(" A&B B& A");
+        expect(generateDiamond("H").replace(new RegExp("\r?\n", "g"), "&")).toBe(
+            "       A&      B B&     C   C&    D     D&   E       E&  F         F& G           G&H             H& G           G&  F         F&   E       E&    D     D&     C   C&      B B&       A"
+        );
+        expect(generateDiamond("R").replace(new RegExp("\r?\n", "g"), "&")).toBe(
+            "                 A&                B B&               C   C&              D     D&             E       E&            F         F&           G           G&          H             H&         I               I&        J                 J&       K                   K&      L                     L&     M                       M&    N                         N&   O                           O&  P                             P& Q                               Q&R                                 R& Q                               Q&  P                             P&   O                           O&    N                         N&     M                       M&      L                     L&       K                   K&        J                 J&         I               I&          H             H&           G           G&            F         F&             E       E&              D     D&               C   C&                B B&                 A"
+        );
     });
 });
--- src/pages/Main/utils.ts
+++ src/pages/Main/utils.ts
@@ -1,3 +1,22 @@
+const startingCharCode = "A".charCodeAt(0);
+
+const padding = (length: number) => " ".repeat(length);
+
+export const addLine = (index: number, length: number) => {
+    if (length - index < 1 || index < 0) {
+        return "";
+    }
+
+    const letter = String.fromCharCode(startingCharCode + index);
+    const leftPadding = padding(length - index - 1);
+
+    if (index === 0) {
+        return `${leftPadding}${letter}`;
+    }
+
+    return `${leftPadding}${letter}${padding(index * 2 - 1)}${letter}`;
+};
+
 export const generateDiamond = (letter: string) => {
     if (letter === "") {
         return "";
@@ -6,4 +25,11 @@ export const generateDiamond = (letter: string) => {
     if (letter === "A") {
         return "A";
     }
+
+    const charCode = letter.charCodeAt(0);
+    const length = charCode - startingCharCode + 1;
+    const upperHalf = Array.from(Array(length).keys()).map((index) => addLine(index, length));
+    const lowerHalf = upperHalf.slice(0, upperHalf.length - 1).reverse();
+
+    return [...upperHalf, ...lowerHalf].join("\n");
 };
